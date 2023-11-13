3b7113fecc8a4c381c30d4b87e7f526dedd5d2e3
Moon-YJ <tunjeong@gmail.com>
2023-11-13 23:57:24 +0900

    첫화면 관련 조건문 추가

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const box = document.querySelector('section.box');
 const btns = box.querySelectorAll('ul.btns li');
-// 첫화면에 배경 색상 적용
-const text = btns[0].innerText;
-box.style.backgroundColor = text;
 
 // 이벤트문
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', (e) => {
-		if (e.currentTarget.classList.contains('on')) return;
+		if (
+			e.currentTarget.classList.contains('on') &&
+			getComputedStyle(box).backgroundColor !== 'rgb(128, 128, 128)'
+		)
+			return;
 		activation(btns, idx);
 		changeBg(btns, idx);
 	});
@@ -15,7 +16,6 @@ btns.forEach((btn, idx) => {
 
 // 활성화함수
 const activation = (arr, idx) => {
-	console.log('activation');
 	arr.forEach((el) => {
 		el.classList.remove('on');
 	});
@@ -24,7 +24,6 @@ const activation = (arr, idx) => {
 
 // 박스 색상 변경함수
 const changeBg = (arr, idx) => {
-	console.log('changeBg');
 	const txt = arr[idx].innerText;
 	box.style.backgroundColor = txt;
 };
