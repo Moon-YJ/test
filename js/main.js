6372918479af1dcaa9eec422dd2b47d058d438b0
Moon-YJ <8768moon@naver.com>
2023-11-13 18:19:12 +0900

    버튼 클릭시 함수 재호출 방지

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 const box = document.querySelector('section.box');
 const btns = box.querySelectorAll('ul.btns li');
+// 첫화면에 배경 색상 적용
+const text = btns[0].innerText;
+box.style.backgroundColor = text;
+
 // 이벤트문
 btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => {
+	btn.addEventListener('click', (e) => {
+		if (e.currentTarget.classList.contains('on')) return;
 		activation(btns, idx);
 		changeBg(btns, idx);
 	});
 });
+
 // 활성화함수
 const activation = (arr, idx) => {
+	console.log('activation');
 	arr.forEach((el) => {
 		el.classList.remove('on');
 	});
 	arr[idx].classList.add('on');
 };
+
 // 박스 색상 변경함수
 const changeBg = (arr, idx) => {
+	console.log('changeBg');
 	const txt = arr[idx].innerText;
 	box.style.backgroundColor = txt;
 };
