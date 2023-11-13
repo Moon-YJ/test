4bb4947d3e8fd551871e3c766221b15f047b5d38
Moon-YJ <8768moon@naver.com>
2023-11-13 18:11:43 +0900

    함수 분리

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 const box = document.querySelector('section.box');
 const btns = box.querySelectorAll('ul.btns li');
-
+// 이벤트문
 btns.forEach((btn, idx) => {
-	btn.addEventListener('click', (e) => {
-		btns.forEach((el) => {
-			el.classList.remove('on');
-		});
-		e.currentTarget.classList.add('on');
-		const txt = btns[idx].innerText;
-		box.style.backgroundColor = txt;
+	btn.addEventListener('click', () => {
+		activation(btns, idx);
+		changeBg(btns, idx);
 	});
 });
+// 활성화함수
+const activation = (arr, idx) => {
+	arr.forEach((el) => {
+		el.classList.remove('on');
+	});
+	arr[idx].classList.add('on');
+};
+// 박스 색상 변경함수
+const changeBg = (arr, idx) => {
+	const txt = arr[idx].innerText;
+	box.style.backgroundColor = txt;
+};
